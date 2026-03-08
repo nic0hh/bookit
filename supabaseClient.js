b7d1cda9358677f3e3cda031097068a90cc70f3a
nicoh <nico.hedman@gmail.com>
2026-03-08 18:27:57 +0100

    Fix supaabase redirect url

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON = process.env.EXPO_PUBLIC_SUPABASE_ANON;
@@ -8,6 +9,7 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    storage: AsyncStorage, // <- required for React Native
+    storage: Platform.OS === 'web' ? undefined : AsyncStorage,
+    flowType: 'pkce',
   },
 });
